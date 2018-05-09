f22e7e980a21c357eeb4a14fd424267b26e1a4d5
gabroon <moh_webdev@hotmail.com>
2018-05-09 11:58:50 +0100

    added comments + extra test cases

--- Visual C#/homework/SuperSimpleStockMarketTest.cs
+++ Visual C#/homework/SuperSimpleStockMarketTest.cs
@@ -10,6 +10,10 @@ namespace SuperSimpleStockMarket
     [TestFixture]
     class SuperSimpleStockMarketTest
     {
+        
+        /// <summary>
+        /// Tests calculating the dividend yield for a stock when the price is an 11 digit number , is zero, is negative and is within a normal range
+        /// </summary>
         [TestCase]
         public void calculateDividendYield()
         {
@@ -20,48 +24,75 @@ namespace SuperSimpleStockMarket
             Assert.AreEqual(0.08, stockPOP.calculateDividendYield(100));
             Assert.AreEqual(0, stockPOP.calculateDividendYield(-10));
         }
-
+        
+        /// <summary>
+        /// Tests calculating the P/E Ratio for a stock when the price is an 11 digit number , is zero, is negative and is within a normal range
+        /// </summary>
         [TestCase]
         public void calculatePERatio()
         {
-            //test common first
             Stock stockPOP = new Stock("POP", StockType.Common, 8, 0, 100);
             Assert.AreEqual(125000000000, stockPOP.calcualtePERatio(10000000000));
             Assert.AreEqual(0, stockPOP.calcualtePERatio(0));
             Assert.AreEqual(1250, stockPOP.calcualtePERatio(100));
             Assert.AreEqual(0, stockPOP.calcualtePERatio(-10));
         }
-
+        
+        /// <summary>
+        /// Tests selling a stock when the quantity and price are zero , negative and postive numbers. 
+        ///The test is done by checking if the trade exists in the list of trades.
+        /// </summary>
         [TestCase]
         public void sellStock()
         {
-            //test common first
             Stock stockPOP = new Stock("POP", StockType.Common, 8, 0, 100);
+              //should record a trade 
             stockPOP.sellStock(10, 100);
             Assert.AreEqual(1,stockPOP.listOfTrades.Count);
+            //should not record a trade 
             stockPOP.sellStock(0, 0);
             Assert.AreEqual(1, stockPOP.listOfTrades.Count);
+             //should not record a trade 
             stockPOP.sellStock(-10, -500);
             Assert.AreEqual(1, stockPOP.listOfTrades.Count);
+             //should not record a trade 
+            stockPOP.sellStock(10, -500);
+            Assert.AreEqual(1, stockPOP.listOfTrades.Count);
+             //should record a trade 
             stockPOP.sellStock(10, 500);
             Assert.AreEqual(2, stockPOP.listOfTrades.Count);
         }
 
+          /// <summary>
+        /// Tests buying a stock when the quantity and price are zero , negative and postive numbers. 
+        ///The test is done by checking if the trade exists in the list of trades.
+        /// </summary>
         [TestCase]
         public void buyStock()
         {
             //test common first
             Stock stockPOP = new Stock("POP", StockType.Common, 8, 0, 100);
+             //should record a trade 
             stockPOP.buyStock(10, 100);
             Assert.AreEqual(1, stockPOP.listOfTrades.Count);
+             //should not record a trade 
             stockPOP.buyStock(0, 0);
             Assert.AreEqual(1, stockPOP.listOfTrades.Count);
+             //should not record a trade 
             stockPOP.buyStock(-10, -500);
             Assert.AreEqual(1, stockPOP.listOfTrades.Count);
+              //should not record a trade 
+            stockPOP.buyStock(10, -500);
+            Assert.AreEqual(1, stockPOP.listOfTrades.Count);
+             //should  record a trade 
             stockPOP.buyStock(10, 500);
             Assert.AreEqual(2, stockPOP.listOfTrades.Count);
         }
-
+        
+        
+        /// <summary>
+        /// Tests calculating the Volume Weighted Stock Price for two situations when the stock has 3 trades and when the stock has no trades at all.
+        /// </summary>
         [TestCase]
         public void calculateVolumeWeightedStockPrice()
         {
@@ -76,10 +107,13 @@ namespace SuperSimpleStockMarket
             Assert.AreEqual(0, stockPOP2.calculateVolumeWeightedStockPrice());
         }
 
-
+         /// <summary>
+        /// Test calculating the All share index in two situations when the the list of stocks is not empty and when it is empty
+        /// </summary>
         [TestCase]
         public void calculateAllShareIndex()
         {
+            
             List<Stock> stocks = new List<Stock>();
             Stock stockTea = new Stock("TEA", StockType.Common, 0, 0, 100);
             Stock stockPOP = new Stock("POP", StockType.Common, 8, 0, 100);
@@ -107,6 +141,10 @@ namespace SuperSimpleStockMarket
             }
 
             Assert.AreEqual(3.0170881682725815432356308547584m, GCBEGeneralMethods.getGCBEAllShareIndex(stocks));
+            
+            //testing the empty stock list
+             List<Stock> emptyStocks = new List<Stock>();
+             Assert.AreEqual(0, GCBEGeneralMethods.getGCBEAllShareIndex(stocks));
 
         }
 
